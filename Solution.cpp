99487c46038f730d1a4b8be26bfd62cab07c49c7
pents <dima.d.96@mail.ru>
2016-02-14 02:44:00 +0300

    modified

--- Solution.cpp
+++ Solution.cpp
@@ -22,21 +22,23 @@ int a[] = {1,5,2,1,4,0};
 int solution(vector<int> &A)
 {
 
-		vector <int> start;
-		vector <int> end;
+		vector <int> start(A.size());
+		vector <int> end(A.size());
 		vector < pair <int, int> > same;
 		int collisions = 0;
-    	for (int i = 0; i < A.size(); i++)
+		for (int i = 0; i < (int)A.size(); i++)
 		{
-			 start.push_back(i - A[i]);
-			 end.push_back(i + A[i]);
+			 start[i] = i - A[i];
+			 end[i] =  i + A[i];
+			// start.push_back(i - A[i]);
+			// end.push_back(i + A[i]);
 		}
-		for (int i = 0; i < A.size(); i++)
+		for (int i = 0; i < (int)A.size(); i++)
 		{
-			for (int j = 0; j < end.size(); j++)
+			for (int j = 0; j < (int)end.size(); j++)
 			{
-				pair<int,int> p = make_pair(i, j);
-			   	if (i == j || find(same.begin(), same.end(), p)!=same.end()) continue;
+				pair<int, int> p = make_pair(i, j);
+				if (i == j || find(same.begin(), same.end(), p)!=same.end()) continue;
 				if ((start[j] <= end[i] && j > i) || (start[j] >= end[i] && j < i))
 				{
 					collisions ++;
